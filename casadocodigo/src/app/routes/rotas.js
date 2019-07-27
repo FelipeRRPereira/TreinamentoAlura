5bcd20ca03c5dcb02ad9f7c982294cfe5bfe4013
Felipe Pereira <felipe.pereira@lighthouseit.com.br>
2019-07-27 17:32:18 -0300

    Add obj livro vazio p/ novo form, add get form id.

--- casadocodigo/src/app/routes/rotas.js
+++ casadocodigo/src/app/routes/rotas.js
@@ -30,8 +30,22 @@ module.exports = (app) => {
     });
     
     app.get('/livros/form', function(req, resp) {
-        resp.marko(require('../views/livros/form/form.marko'));
+        resp.marko(require('../views/livros/form/form.marko'), { livro: {} });
     });
+
+    app.get('/livros/form/:id', function(req, resp) {
+        const { id } = req.params;
+        const livroDao = new LivroDao(db);
+        
+        livroDao.buscaPorId(id)
+            .then(livro =>
+                resp.marko(
+                    require('../views/livros/form/form.marko'),
+                    { livro }
+                )
+            )
+            .catch(erro => console.log(erro));
+    })
     
     app.post('/livros', function(req, resp) {
         console.log(req.body);
