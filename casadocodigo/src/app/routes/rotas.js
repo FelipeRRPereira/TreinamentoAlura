ab59e0200528298564727b4a3ccb25a195dcec2b
Felipe Pereira <felipe.pereira@lighthouseit.com.br>
2019-07-27 16:41:25 -0300

    Adicionado rota de delete.

--- casadocodigo/src/app/routes/rotas.js
+++ casadocodigo/src/app/routes/rotas.js
@@ -16,6 +16,7 @@ module.exports = (app) => {
             `
         );
     });
+    
     app.get('/livros', function (req, resp) {
         const livroDao = new LivroDao(db);
         livroDao.lista() 
@@ -27,14 +28,24 @@ module.exports = (app) => {
             ))
             .catch(erro => console.log(erro)); 
     });
+    
     app.get('/livros/form', function(req, resp) {
         resp.marko(require('../views/livros/form/form.marko'));
     });
+    
     app.post('/livros', function(req, resp) {
         console.log(req.body);
         const livroDao = new LivroDao(db);
         livroDao.adiciona(req.body)
             .then(resp.redirect('/livros'))
+            .catch(erro => console.log(erro));
+    });
+
+    app.delete('/livros/:id', function(req, resp) {
+        const id = req.params.id;
+        const livroDao = new LivroDao(db);
+        livroDao.remove(id)
+            .then(() => resp.status(200).end())
             .catch(erro => console.log(erro));
     });
 }
