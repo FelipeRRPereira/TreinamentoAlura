2d27990f3ba74eafa90fbca36564e646b80fe312
Felipe Pereira <felipe.pereira@lighthouseit.com.br>
2019-07-27 16:53:41 -0300

    Add middleware express p/ arquivos estaticos.

--- casadocodigo/src/config/custom-express.js
+++ casadocodigo/src/config/custom-express.js
@@ -3,8 +3,8 @@ require('marko/express');
 
 const express = require('express');
 const app = express();
-
 const bodyParser = require('body-parser');
+app.use('/estatico', express.static('src/app/public'));
 app.use(bodyParser.urlencoded({
     extended: true
 }));
