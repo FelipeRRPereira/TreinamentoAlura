1615b4ac5ce5b0c937aaa7c72ffd9ba714585e3b
Felipe Pereira <felipe.pereira@lighthouseit.com.br>
2019-08-03 15:51:20 -0300

    Alterado rota para chamar model de validações.

--- casadocodigo/src/app/routes/rotas.js
+++ casadocodigo/src/app/routes/rotas.js
@@ -4,7 +4,7 @@ const livroController = new LivroController();
 const BaseController = require('../controller/base-controlador');
 const baseController = new BaseController();
 
-const { check } = require('express-validator/check');
+const Livro = require('../model/livro');
 
 module.exports = (app) => {
     const rotasBase = BaseController.rotas();
@@ -14,10 +14,7 @@ module.exports = (app) => {
     app.get(rotasLivro.lista, livroController.lista());
     app.get(rotasLivro.cadastro, livroController.formularioCadastro());
     app.get(rotasLivro.edicao, livroController.formularioEdicao())
-    app.post(rotasLivro.lista, [
-        check('titulo').isLength({ min: 5 }).withMessage('O título precisa de no mínimo 5 caracteres!'),
-        check('preco').isCurrency().withMessage('O preço precisa ter um valor monetário válido!')
-    ], livroController.cadastra());
+    app.post(rotasLivro.lista, Livro.validacoes(), livroController.cadastra());
     app.put(rotasLivro.lista, livroController.edita());
     app.delete(rotasLivro.delecao, livroController.remove());
 }
