789c023c85d05f9936924a67a8cace291290616e
Felipe Pereira <felipe.pereira@lighthouseit.com.br>
2019-08-04 17:08:45 -0300

    Simplificando arquivos de rotas onde a somente as chamadas para os novos arquivos de rotas.

--- casadocodigo/src/app/routes/rotas.js
+++ casadocodigo/src/app/routes/rotas.js
@@ -1,20 +1,7 @@
-const LivroController = require('../controller/livro-controller');
-const livroController = new LivroController();
-
-const BaseController = require('../controller/base-controlador');
-const baseController = new BaseController();
-
-const Livro = require('../model/livro');
+const livroRotas = require('./livro-rotas');
+const baseRotas = require('./base-rotas');
 
 module.exports = (app) => {
-    const rotasBase = BaseController.rotas();
-    const rotasLivro = LivroController.rotas();
-
-    app.get(rotasBase.home, baseController.home());
-    app.get(rotasLivro.lista, livroController.lista());
-    app.get(rotasLivro.cadastro, livroController.formularioCadastro());
-    app.get(rotasLivro.edicao, livroController.formularioEdicao())
-    app.post(rotasLivro.lista, Livro.validacoes(), livroController.cadastra());
-    app.put(rotasLivro.lista, livroController.edita());
-    app.delete(rotasLivro.delecao, livroController.remove());
-}
+    baseRotas(app);
+    livroRotas(app);
+};
